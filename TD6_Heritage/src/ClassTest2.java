b32eae1ca61a97ef3ebfb5a148244e278ce6b4a0
aleks <aleks@LAPTOP-RTPKGBET>
2022-10-27 11:23:42 +0200

    Corrections

--- TD6_Heritage/src/ClassTest2.java
+++ TD6_Heritage/src/ClassTest2.java
@@ -1,7 +1,6 @@
 	/**
 	 * @author aleks
 	 * @comments classe de test pour la classe Chat dérivée de la classe Felin, implémentant l'interface Domesticable
-	 * @param args
 	 * 
 	 */
 
@@ -10,6 +9,8 @@ public class ClassTest2 {
 	public static void main(String[] args) {
 		Chat C = new Chat("Siamois", 4, true);
 		C.présente();
+		C.domestiquer("Je m'apelle Felix");
+		System.out.println(C.nom());
 
 	}
 
