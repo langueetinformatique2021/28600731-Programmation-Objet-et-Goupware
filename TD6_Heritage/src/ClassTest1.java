fe36982c9b2a5897d8a28ace6f43d15b51ceb1d5
aleks <aleks@LAPTOP-RTPKGBET>
2022-11-09 16:43:13 +0100

    rangement

--- TD6_Heritage/src/ClassTest1.java
+++ TD6_Heritage/src/ClassTest1.java
@@ -1,7 +1,7 @@
 
 public class ClassTest1 {
 	/**
-	 * @author aleks
+	 * @author Alexandra DEMKOVIC
 	 * @comments test de la classe Felin reliée à la classe Animal 
 	 * @return présente() le felin avec ses caractéristiques données
 	 */
