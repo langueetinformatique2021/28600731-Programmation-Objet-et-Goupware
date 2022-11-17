1bbfd5b407f22b328dc5370e3e38d286b0bec9da
aleks <aleks@172.23.205.66>
2022-11-17 10:30:37 +0100

    TD7_ex3 afficher la date et l'heure courante

---
+++ TD7_HeritageClass2/src/DerivationClassesAPI/DateHeureCourante.java
@@ -0,0 +1,18 @@
+package DerivationClassesAPI;
+
+/**
+ * @author Alexandra Demkovic
+ * Afficher la date et l'heure courante avec la méthode Date()
+ */
+import java.util.Date;
+
+public class DateHeureCourante {
+
+	public static void main(String[] args) {
+		   Date date = new Date();
+
+		   System.out.println(date);
+
+	}
+
+}
