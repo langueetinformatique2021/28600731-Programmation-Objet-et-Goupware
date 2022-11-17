272363f5963433075804199bca079715bb8b59e0
aleks <aleks@172.23.205.66>
2022-11-17 10:37:53 +0100

    TD7 - ex2 _ Date et Heure Gregorian Calendar

--- TD7_HeritageClass2/src/DerivationClassesAPI/DateHeureGC.java
+++ TD7_HeritageClass2/src/DerivationClassesAPI/DateHeureGC.java
@@ -1,5 +1,10 @@
 package DerivationClassesAPI;
 
+/**
+ * @author Alexandra Demkovic
+ * Afficher la date et l'heure de la création d'un objet de type GregorianCalendar
+ */
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -8,7 +13,7 @@ public class DateHeureGC {
 
 	public static void main(String[] args) {
 		   GregorianCalendar calendar = new GregorianCalendar(2022, Calendar.NOVEMBER , 17);
-		   Date date = calendar.getTime();
+		   Date date = calendar.getTime();  //getTime() pour afficher la date et l'heure
 		   calendar.set(Calendar.HOUR, 10);
 		   calendar.set(Calendar.MINUTE, 13);
 		   calendar.set(Calendar.SECOND, 23);
@@ -17,8 +22,6 @@ public class DateHeureGC {
 		   
 		   System.out.println("Année : " + calendar.get(Calendar.YEAR));
 		   System.out.println("Mois : " + calendar.get(Calendar.MONTH));
-		   System.out.println("Semaine du mois : " + calendar.get(Calendar.WEEK_OF_MONTH));// Semaine de ce mois ?
-
 		   System.out.println("Jour : " + calendar.get(Calendar.DAY_OF_MONTH));
 
 		   System.out.println("Heures : " + calendar.get(Calendar.HOUR));
