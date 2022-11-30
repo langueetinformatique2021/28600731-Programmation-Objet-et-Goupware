1dc1765b1b8a12f8d681fc0389fcc7e758227968
AleksandraDkc <114649608+AleksandraDkc@users.noreply.github.com>
2022-11-30 14:20:20 +0100

    TD8 _ Exercice 1 sur les divisions avec l'instruction try-catch et capture de l'exception levée

---
+++ TD8_LancerDes/Division.java
@@ -0,0 +1,29 @@
+package TD8_LancerDes;
+
+/**
+ * 
+ * @author Alexandra DKC
+ * TD8 _ exercies 1 et 2 : captuer l'exception levée avec try-catch
+ */
+
+public class Division {
+
+	public static void main(String[] args) {
+			int i = 1000, j;
+			try {
+				do {
+					i--;
+					j = 1 / i;
+				} while (true);
+			}	
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
+
+/**
+ * Message renvoyé : l'exception est la division par zéro 
+ * " / by zero "
+ */
+
