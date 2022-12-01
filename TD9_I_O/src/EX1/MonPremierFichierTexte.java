a558b99a83d41322221efb681a08622adb3c5bc3
AleksandraDkc <114649608+AleksandraDkc@users.noreply.github.com>
2022-12-01 10:24:33 +0100

    TD9 EX1 et 2

--- TD9_I_O/src/EX1/MonPremierFichierTexte.java
+++ TD9_I_O/src/EX1/MonPremierFichierTexte.java
@@ -1,6 +1,8 @@
 package EX1;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -11,10 +13,11 @@ public class MonPremierFichierTexte {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String nomFichier = "C:\\Users\\aleks\\git\\28600731-Programmation-Objet-et-Goupware\\TD9_I_O\\src\\EX1\\Nom.txt";
 
 		 try {
 			 
-			 PrintWriter pr = new PrintWriter(new File("C:\\Users\\aleks\\eclipse-workspace\\TD9_I_O\\src\\EX1\\Nom.txt)"));
+			 PrintWriter pr = new PrintWriter(new File(nomFichier));
 				pr.println("Alexandra");
 				pr.println("Marceau");
 				pr.println("Yuyan");
@@ -28,5 +31,28 @@ public class MonPremierFichierTexte {
 			 e.printStackTrace();
 			 }
 		 }
+	
+	public static boolean Verifier(String nomFichier) {
+		try {
+			BufferedReader br = new BufferedReader(
+					new FileReader(new File(nomFichier)));
+			if (br.readLine().equals("Alexandra") == false)
+				return false;
+			if (br.readLine().equals("Marceau") == false)
+				return false;
+			if (br.readLine().equals("Yuyan") == false)
+				return false;
+			if (br.readLine().equals("Didier") == false)
+				return false;
+			if (br.readLine().equals("Romane") == false)
+				return false;
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return true;
+		
+	}
+
 }
 
